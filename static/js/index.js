05ff62b657afdd65257248f22af1553542eadccf
adtg-sim-real <corl24adtg@gmail.com>
2024-06-01 13:19:35 -0400

    update

--- static/js/index.js
+++ static/js/index.js
@@ -1,78 +1,76 @@
 window.HELP_IMPROVE_VIDEOJS = false;
 
 var INTERP_BASE = "./static/interpolation/stacked";
+var INTERP_BASE_NEW = "./static/interpolation/new_stacked";
 var NUM_INTERP_FRAMES = 240;
 
 var interp_images = [];
-function preloadInterpolationImages() {
+var interp_base_interp_images = [];
+
+function preloadInterpolationImages(base, interp_images) {
   for (var i = 0; i < NUM_INTERP_FRAMES; i++) {
-    var path = INTERP_BASE + '/' + String(i).padStart(6, '0') + '.jpg';
+    var path = base + '/' + String(i).padStart(6, '0') + '.jpg';
     interp_images[i] = new Image();
     interp_images[i].src = path;
   }
 }
 
-function setInterpolationImage(i) {
+function setInterpolationImage(interp_images, wrapper_id, i) {
   var image = interp_images[i];
   image.ondragstart = function() { return false; };
   image.oncontextmenu = function() { return false; };
-  $('#interpolation-image-wrapper').empty().append(image);
+  $('#' + wrapper_id).empty().append(image);
 }
 
-
 $(document).ready(function() {
-    // Check for click events on the navbar burger icon
-    $(".navbar-burger").click(function() {
-      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-      $(".navbar-burger").toggleClass("is-active");
-      $(".navbar-menu").toggleClass("is-active");
+  // Navbar burger toggle
+  $(".navbar-burger").click(function() {
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
+  });
 
+  // Carousel initialization
+  var options = {
+    slidesToScroll: 1,
+    slidesToShow: 3,
+    loop: true,
+    infinite: true,
+    autoplay: false,
+    autoplaySpeed: 3000,
+  }
+
+  var carousels = bulmaCarousel.attach('.carousel', options);
+
+  for (var i = 0; i < carousels.length; i++) {
+    carousels[i].on('before:show', state => {
+      console.log(state);
     });
+  }
 
-    var options = {
-			slidesToScroll: 1,
-			slidesToShow: 3,
-			loop: true,
-			infinite: true,
-			autoplay: false,
-			autoplaySpeed: 3000,
-    }
-
-		// Initialize all div with carousel class
-    var carousels = bulmaCarousel.attach('.carousel', options);
-
-    // Loop on each carousel initialized
-    for(var i = 0; i < carousels.length; i++) {
-    	// Add listener to  event
-    	carousels[i].on('before:show', state => {
-    		console.log(state);
-    	});
-    }
-
-    // Access to bulmaCarousel instance of an element
-    var element = document.querySelector('#my-element');
-    if (element && element.bulmaCarousel) {
-    	// bulmaCarousel instance is available as element.bulmaCarousel
-    	element.bulmaCarousel.on('before-show', function(state) {
-    		console.log(state);
-    	});
-    }
-
-    /*var player = document.getElementById('interpolation-video');
-    player.addEventListener('loadedmetadata', function() {
-      $('#interpolation-slider').on('input', function(event) {
-        console.log(this.value, player.duration);
-        player.currentTime = player.duration / 100 * this.value;
-      })
-    }, false);*/
-    preloadInterpolationImages();
-
-    $('#interpolation-slider').on('input', function(event) {
-      setInterpolationImage(this.value);
+  var element = document.querySelector('#my-element');
+  if (element && element.bulmaCarousel) {
+    element.bulmaCarousel.on('before-show', function(state) {
+      console.log(state);
     });
-    setInterpolationImage(0);
-    $('#interpolation-slider').prop('max', NUM_INTERP_FRAMES - 1);
+  }
 
-    bulmaSlider.attach();
+  // Preload images
+  preloadInterpolationImages(INTERP_BASE, interp_images);
+  preloadInterpolationImages(INTERP_BASE_NEW, interp_base_interp_images);
 
-})
+  // Old interpolation slider
+  $('#old-interpolation-slider').on('input', function(event) {
+    setInterpolationImage(interp_images, 'old-interpolation-image-wrapper', this.value);
+  });
+  setInterpolationImage(interp_images, 'old-interpolation-image-wrapper', 0);
+  $('#old-interpolation-slider').prop('max', NUM_INTERP_FRAMES - 1);
+
+  // New interpolation slider
+  $('#new-interpolation-slider').on('input', function(event) {
+    setInterpolationImage(interp_base_interp_images, 'new-interpolation-image-wrapper', this.value);
+  });
+  setInterpolationImage(interp_base_interp_images, 'new-interpolation-image-wrapper', 0);
+  $('#new-interpolation-slider').prop('max', NUM_INTERP_FRAMES - 1);
+
+  bulmaSlider.attach();
+});
